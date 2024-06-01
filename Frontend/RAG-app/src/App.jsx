8b244db2b4366354994e15be9c5ae053b3af2b75
vaibhavg4651 <vaibhav.gupta.2021@msit.in>
2024-06-01 20:49:24 +0530

    UI update

--- Frontend/RAG-app/src/App.jsx
+++ Frontend/RAG-app/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar />
+      <div className='Text-input'>
+        <div className='input-bar'>
+          <input  type="text" placeholder='Send a message...'/>
+          <img src="" alt="" />
+        </div>
+      </div>
     </>
   )
 }
--- Frontend/RAG-app/src/Navbar.jsx
+++ Frontend/RAG-app/src/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import Logo from "./assets/Logo.svg"
 
 const Navbar = () => {
   return (
     <div>
         <div className='navbar'>
-            <section>
+            <section className='logo'>
+              <div>
+                <img src={Logo} alt="Logo" />
+              </div>
             </section>
             <section>
-
+              <div>
+                <label htmlFor="Upload file"></label>
+                <input name="pdf"  type="file" placeholder='Upload File'/>
+              </div>
             </section>
         </div>
     </div>
